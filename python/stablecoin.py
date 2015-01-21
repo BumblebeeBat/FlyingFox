47e6f8e17de02455ba0bb97058f916d52cad9a18
zack <zack.bitcoin@gmail.com>
2015-01-21 13:41:15 -0600

    added space

--- python/stablecoin.py
+++ python/stablecoin.py
@@ -46,6 +46,6 @@ def test():
         example_pm={'results':[p], 'predictions':['price of BTC in USD within '+ str(margin[0])+', ' + str(margin[1])], 'functions':[str(margin[1]-margin[0])+' * '+str(margin[0]) +' + 1 / '+ str(margin[0]) +' *'], 'output states':['stable-coin', 'vol-coin'], 'algorithm':[0, ['stable-coin']]}
         print('volshare '+str(share_value(example_pm, 'vol-coin')))
         print('stableshare '+str(share_value(example_pm, 'stable-coin')))
-        print('value stableshare in USD' +str(share_value(example_pm, 'stable-coin')*(p*(margin[1]-margin[0])+margin[0])))
+        print('value stableshare in USD ' +str(share_value(example_pm, 'stable-coin')*(p*(margin[1]-margin[0])+margin[0])))
 if __name__=='__main__': #for windows
     test()
