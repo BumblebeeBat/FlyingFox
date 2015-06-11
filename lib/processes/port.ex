86df8bc93be8602a50031fa72eb871f1a114f284
zack <zack.bitcoin@gmail.com>
2015-06-11 13:09:57 -0700

    optimized the round robin port selector

--- lib/processes/port.ex
+++ lib/processes/port.ex
@@ -7,18 +7,18 @@ defmodule Port do
 	def internal do elem(port, 0) end
 	def external do elem(port, 1) end
 	def handle_call(:port, _from, d) do {:reply, d, d} end
-	def round_robin(current, range) do
-		cond do
-			current >= range - 1 -> current - range + 2
-			true -> current + 1
-		end
+	def round_robin do
+		:random.seed(:erlang.now)
+		trunc(Constants.max_nodes*:random.uniform)
+		#cond do
+		#	current >= range - 1 -> current - range + 2
+		#	true -> current + 1
+		#end
 	end
 	def next do GenServer.cast(@name, :next) end
 	def handle_cast(:next, d) do
-		IO.puts("d #{inspect d}")
-		q = Constants.tcp_port
-		o = round_robin(elem(d, 1) - q, Constants.max_nodes) + q
-		IO.puts("q o #{inspect q} #{inspect o}")
-		{:noreply, {elem(d, 0), o}}
+		#q = Constants.tcp_port
+		#o = round_robin(elem(d, 1) - q, Constants.max_nodes) + q
+		{:noreply, {elem(d, 0), round_robin+Constants.tcp_port}}
 	end	
 end
