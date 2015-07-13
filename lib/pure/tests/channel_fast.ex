5ac5adbead0bb9e064dbbdfe6c6ca38ac88fd6df
zack <zack.bitcoin@gmail.com>
2015-07-12 17:46:21 -0700

    test update

--- lib/pure/tests/channel_fast.ex
+++ lib/pure/tests/channel_fast.ex
@@ -1,10 +1,14 @@
 defmodule CF do
 	#odd numbers are one node, even numbers are the other.
+	def peer(port) do %Peer{ip: "localhost", port: port} end
 	def func1 do
 		Cli.new_key
-		Cli.status.pubkey
+		#Cli.status.pubkey
+		Port.port
 	end
-	def func2(key) do#key was the output of fun1
+	def func2(port) do#key was the output of fun1
+		p = peer(port)
+		key = Cli.status(p).pubkey
 		Cli.spend(key, 500000000)
 		Cli.buy_block
 		Cli.to_channel(key, 5000)
