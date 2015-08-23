209b206dd25b8588c316abe9d7ad50624025bbc6
zack <zack.bitcoin@gmail.com>
2015-08-23 12:18:52 -0700

    packer remove redundancy

--- erlang/src/packer.erl
+++ erlang/src/packer.erl
@@ -2,15 +2,16 @@
 %We should add some rules about which atoms can be used with tuples. If peers can trick us into decoding new atoms, they can overflow erlang with too many atoms.
 -module(packer).
 -export([pack/1,unpack/1,test/0]).
-untup(X) when is_tuple(X) -> ["tup"|untup(tuple_to_list(X))];
+-define(KEY, "tUP").
+untup(X) when is_tuple(X) -> [?KEY|untup(tuple_to_list(X))];
 untup(X) when is_list(X) -> lists:map(fun(Z)->untup(Z) end, X);
 untup(X) -> X.
 retup(X) when not is_list(X) -> X;
 retup(X) when is_binary(hd(tl(X))) -> list_to_tuple([list_to_atom(binary_to_list(hd(tl(X))))|lists:map(fun(Z)->retup(Z) end, tl(tl(X)))]);
-retup(X) when hd(X) == "tup" -> list_to_tuple(retup(tl(X)));
+retup(X) when hd(X) == ?KEY -> list_to_tuple(retup(tl(X)));
 retup(X) -> lists:map(fun(Z)->retup(Z) end, X).
 unpack(JSON) -> unpack_helper(jiffy:decode(JSON)).
-unpack_helper(J) when is_list(J) and (hd(J)=="tup") -> 
+unpack_helper(J) when is_list(J) and (hd(J) == ?KEY) -> 
     retup([hd(J)|unpack_helper(tl(J))]);
 unpack_helper(J) when is_list(J) -> lists:map(fun(X) -> unpack_helper(X) end, J);
 unpack_helper(J) -> J.
