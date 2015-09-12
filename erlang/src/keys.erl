344f20d0f0c8f98c64c3e67214db645c469786ae
zack <zack.bitcoin@gmail.com>
2015-09-12 08:52:04 -0700

    fixed packer so it holds binaries as base64 encoded strings

--- erlang/src/keys.erl
+++ erlang/src/keys.erl
@@ -3,7 +3,7 @@
 -module(keys).
 -behaviour(gen_server).
 -export([start_link/0,code_change/3,handle_call/3,handle_cast/2,handle_info/2,init/1,terminate/2, pubkey/0,sign/1,raw_sign/1,load/3,unlock/1,lock/0,status/0,change_password/2,new/1,shared_secret/1]).
--define(LOC(), "keys").
+-define(LOC(), "keys.db").
 -define(SANE(), "sanity").
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
