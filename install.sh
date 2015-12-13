8c4bb6c7910d5030d917ea1498c73f4e8f9571b9
zack <zack.bitcoin@gmail.com>
2015-12-12 16:03:44 -0800

    rebar couldn't compile, so download instead

--- install.sh
+++ install.sh
@@ -3,11 +3,7 @@ if [ -e "rebar" ]
 then
     echo "rebar already installed"
 else
-    git clone git://github.com/rebar/rebar.git rebar_source
-    cd rebar_source/
-    ./bootstrap
-    cp rebar ..
-    cd ..
+    wget https://github.com/rebar/rebar/wiki/rebar && chmod u+x rebar
 fi
 #use rebar to install other dependencies, explained in rebar.config
 ./rebar get
