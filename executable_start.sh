cbe77ac6aab28b3c012ef422f8be8af3aa411eac
zack <zack.bitcoin@gmail.com>
2015-12-12 17:54:54 -0800

    clean up databases before starting the executable.

--- executable_start.sh
+++ executable_start.sh
@@ -1,3 +1,7 @@
+sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
+
 ./rel/flying_fox/bin/flying_fox start
 sleep 5
 ./rel/flying_fox/bin/flying_fox attach
+
+echo "GO TO THIS WEBSITE -------> http://localhost:3011/main.html"
