fca4dace7ec669fe8220f1c2b74299e950cc6f42
zack <zack.bitcoin@gmail.com>
2015-12-12 19:09:11 -0800

    changes path

--- executable_start.sh
+++ executable_start.sh
@@ -1,5 +1,3 @@
-sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
-
 ./rel/flying_fox/bin/flying_fox start
 sleep 5
 ./rel/flying_fox/bin/flying_fox attach
--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ else
     curl https://raw.githubusercontent.com/wiki/rebar/rebar/rebar -o rebar
     chmod u+x rebar
 fi
+
+#add erlang run time system to path
+PATH=./rel/flying_fox/erts-6.4/bin:$PATH
+
 #use rebar to install other dependencies, explained in rebar.config
 ./rebar get
 ./rebar compile
 
+sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
